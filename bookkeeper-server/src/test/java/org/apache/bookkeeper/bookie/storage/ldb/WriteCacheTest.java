57c854848ab3aa49d3859b10ed85750d469571cc
mirko <mirkoleandri110392@gmail.com>
2021-08-11 11:07:37 +0200

    Finalmente iniziamo a correggere la sintassi

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/WriteCacheTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/WriteCacheTest.java
@@ -37,7 +37,7 @@ public class WriteCacheTest {
         //Scegliamo i parametri per testare il metodo in test, vogliamo andare a testare i metodi put e clear oltre al costruttore
         //questi prendono come argomenti solamente un long e un allocator, (il metodo put prende anche due id che metterò hardcoded)
         //ho deciso di aggiungere l'atttributo bufferSize per vedere se aggiungendo entry di dimensione diversa da quella della cache ci sarebbero stati problemi
-		return Arrays.asList(new Object[][] {
+		return Array.asList(new Object[][] {
 			{UnpooledByteBufAllocator.DEFAULT, 10*1024,1024},{UnpooledByteBufAllocator.DEFAULT, 1000 * 1024,1024},  {UnpooledByteBufAllocator.DEFAULT, 0,01 * 1024,1024}, {UnpooledByteBufAllocator.DEFAULT, 10*1024,512}, {UnpooledByteBufAllocator.DEFAULT, 10*1024,2048}
 		});
 	}
@@ -84,9 +84,9 @@ public class WriteCacheTest {
             }
         }
         for(int c = 0; c <10; c ++) {
-             assertEquals(entry, get(1));
+             assertEquals(entry, cache.get(1));
              assertEquals((cache.get(c,c)),entry);
-             cahce.deleteLedger(c);
+             cache.deleteLedger(c);
              assertEquals(cache.get(c,c), null);
             }
     }
